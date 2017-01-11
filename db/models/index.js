44e98d00c83e5d3b3ae4dd0cbc23b5346b933baf
Kathryn Middleton <kmiddleton14@gmail.com>
2017-01-11 17:13:57 -0500

    added the proper associations

--- db/models/index.js
+++ db/models/index.js
@@ -10,6 +10,28 @@ const Order = require('./order')
 const WeatherBase = require('./weatherbase')
 const WeatherExtra = require('./weatherextra')
 const Package = require('./package')
+const Review = require('./review')
+const OrderPackage = require('./orderpackage')
 
-module.exports = {User, City, Order, WeatherBase, WeatherExtra, Package}
+//associations
+
+//create pivot table called packageextra
+Package.belongsToMany(WeatherExtra, { through: 'packageextra' })
+WeatherExtra.belongsToMany(Package, { through: 'packageextra' })
+
+Package.belongsTo(WeatherBase, {as: 'base'})
+
+Review.belongsTo(Package, {as: 'package'})
+Review.belongsTo(User, {as: 'author'})
+
+
+Order.belongsToMany(Package, { through: 'orderpackage' })
+Package.belongsToMany(Order, { through: 'orderpackage' })
+
+Order.belongsTo(User, {as: 'user'})
+Order.belongsTo(City, {as: 'ship_city'})
+//Order.belongsTo(Payment, {as: 'payment'})
+
+
+module.exports = {User, City, Order, WeatherBase, WeatherExtra, Package, Review}
 
