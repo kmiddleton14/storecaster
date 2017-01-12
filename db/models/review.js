9ee0810f032421b0fec80dd525da955631097f71
Harmony D <antancia@gmail.com>
2017-01-12 11:26:01 -0500

    Updated classMethods reference to package_id and also added getByAuthorId method

--- db/models/review.js
+++ db/models/review.js
@@ -11,27 +11,34 @@ const Review = db.define('review', {
     type: Sequelize.STRING
   },
   rating: {
-    type: Sequelize.ENUM(1, 2, 3, 4, 5),
+    type: Sequelize.ENUM('1', '2', '3', '4', '5'),
     allowNull: false
   },
   authorName: {
     type: Sequelize.STRING,
     //if no name is provided, should result to "anonymous"
     defaultValue: 'anonymous',
-    allowNull: false           
+    allowNull: false
   },
 }, {
 
   classMethods: {
     //The caller of this function can calculate the average ratings by dividing total of all ratings by their number
-    getReviewsByPackageId: (pkgId) => {
+    getByPackageId: (pkgId) => {
       return Review.findAll({
         where: {
-          packageId: pkgId
+          package_id: pkgId
+        }
+      })
+    },
+    getByAuthorId: (authorId) => {
+      return Review.findAll({
+        where: {
+          author_id: authorId
         }
       })
     }
-  }  
+  }
 })
 
 module.exports = Review;
