5a7b23aafb817a84441b6f7b6b9baafa54f64267
C.S. Jiang <christine.s.jiang@gmail.com>
2017-01-16 13:58:33 -0500

    changed the entire product display in products row to a link

--- app/components/ProductsRow.jsx
+++ app/components/ProductsRow.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default ({ products=[], pickProduct }) => 
     <div>
      {
       products.map(product => (
-      <li key={product.id}>
-        {
-          product.category && 
-          <div><h1>{product.category}</h1></div>
-        }
-        <h2>{product.name}</h2>
-        <h3>{product.description}</h3>
-        <img 
-          src={product.imageURL} 
-          onClick={() => pickProduct(product)}
-        />
-        <h3>$40.00</h3>
-      </li>
+      <Link key={product.id} to={`/selectedproduct`} onClick={() => pickProduct(product)}>
+        <li>
+          {
+            product.category && 
+            <div><h1>{product.category}</h1></div>
+          }
+          <h2>{product.name}</h2>
+          <h3>{product.description}</h3>
+          <img 
+            src={product.imageURL} 
+          />
+          <h3>$40.00</h3>
+        </li>
+      </Link>
       ))
     }
     </div>
