493c4c834a59a47e172bbea5ec664fc555d057a9
Harmony Dashut <antancia@gmail.com>
2017-01-17 22:40:39 -0500

    Update Products.jsx

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -4,6 +4,7 @@ import ProductsRow from './ProductsRow';
 export const Products = ({ products, pickProduct }) => ( 
   <div>
     <h1>Your Weather, Your Way</h1>
+    <h4>Select Your Starting Package</h4>
     <ProductsRow 
       products={products.packages} 
       pickProduct={pickProduct}
